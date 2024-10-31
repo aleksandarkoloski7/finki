bf38396b9f9e18cecc173f13d5566b6baaea5192
Aleksandar <149061620+aleksandarkoloski7@users.noreply.github.com>
2024-10-31 11:23:05 +0100

    Update 1.java

--- PNAIPS/1.java
+++ PNAIPS/1.java
@@ -1,66 +1,222 @@
-/*За дадена низа од N цели броеви, да се избришат елементите со вредност помала од просекот на сите елементи во низата. 
-На пример за низата 1, 2, 3, 4, 5 просекот е (1 + 2 + 3 + 4 + 5) / 5 = 15 / 5 = 3, што значи треба да се избришат елементите 1 и 2, што значи низата после оваа трансформација ќе биде 3, 4, 5.
-Влез: Првиот број од влезот е бројот на елементи во низата N, а потоа во следниот ред се дадени самите елементи одделени со празно место.
-Излез: Низата пред и после направената трансформација. 
-Input	               Result
-5                   {1,2,3,4,5}
-1 2 3  4 5          {3,4,5}
-*/
+/* Дадена е еднострана поврзана листа чии што јазли содржат по еден String. Дополнително, даден е и уште еден природен број L.
+Во дадената листа потребно е сите јазли коишто содржат String со должина L
+да се поместат на крај на листата, задржувајќи го и оригиналниот распоред на елементите.
+Влез: Во првиот ред од влезот е даден бројот на елементи во листата - N,
+па во следните N редови се дадени самите елементи (String-ови) од листата. На крај, во последниот ред е даден бројот L. 
+Излез: На излез треба да се испечати листата пред и после промената. */
+Input	      Result
+3            Sky->Blue->Cloud  
+Sky          Sky->Blue->Cloud  
+Blue
+Cloud
+5
+Input                Result
+4           Apple->Banana->Pear->Watermelon     
+Apple       Apple->Pear->Watermelon->Banana
+Banana
+Pear
+Watermelon
+6
 
 import java.util.Scanner;
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // vnesi n za niza i polni ja
-        int n = in.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-        }
-        
-       // presmetka na prosek
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += arr[i];
-        }
-        double prosek = sum / n;
-        
-        // pecati ja listata bez promeni
-        System.out.print("{");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i]);
-            if (i < n - 1) { // pecati zapirka do predposledniot broj 
-                System.out.print(",");
-            }
-        }
-        System.out.println("}");
-        
-        // presmetka za brojot na elementi koi se pogolemi i ednakvi na prosekot.
-        int brojac = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] >= prosek) {
-                brojac++;
-            }
-        }
-        
-        // nova niza za tie elementi pogore
-        int[] newarr = new int[brojac];
-        int index = 0;
-        for (int i = 0; i < n; i++) {  
-            if (arr[i] >= prosek) {
-                newarr[index++] = arr[i];
-            }
-        }
-        
-        // pecateneje na transformirana niza
-        System.out.print("{");
-        for (int i = 0; i < brojac; i++) {
-            System.out.print(newarr[i]);
-            if (i < brojac - 1) { 
-                System.out.print(",");
-            }
-        }
-        System.out.println("}");
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
     }
 }
+class SLL<E> {
+    private SLLNode<E> first;
+
+    public SLL() {
+        // Construct an empty SLL
+        this.first = null;
+    }
+
+    public void deleteList() {
+        first = null;
+    }
+
+    public int size() {
+        int listSize = 0;
+        SLLNode<E> tmp = first;
+        while(tmp != null) {
+            listSize++;
+            tmp = tmp.succ;
+        }
+        return listSize;
+    }
+
+    @Override
+    public String toString() {
+        String ret = new String();
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            ret += tmp.element;
+            while (tmp.succ != null) {
+                tmp = tmp.succ;
+                ret += "->" + tmp.element;
+            }
+        } else
+            ret = "Prazna lista!!!";
+        return ret;
+    }
+
+    public void insertFirst(E o) {
+        SLLNode<E> ins = new SLLNode<E>(o, null);
+        ins.succ = first;
+        //SLLNode<E> ins = new SLLNode<E>(o, first);
+        first = ins;
+    }
+
+    public void insertAfter(E o, SLLNode<E> node) {
+        if (node != null) {
+            SLLNode<E> ins = new SLLNode<E>(o, node.succ);
+            node.succ = ins;
+        } else {
+            System.out.println("Dadenot jazol e null");
+        }
+    }
+    public void insertBefore(E o, SLLNode<E> before) {
+
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if(first==before){
+                this.insertFirst(o);
+                return;
+            }
+            //ako first!=before
+            while (tmp.succ != before && tmp.succ!=null)
+                tmp = tmp.succ;
+            if (tmp.succ == before) {
+                tmp.succ = new SLLNode<E>(o, before);;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+    }
+
+    public void insertLast(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = new SLLNode<E>(o, null);
+        } else {
+            insertFirst(o);
+        }
+    }
+
+    public E deleteFirst() {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            first = first.succ;
+            return tmp.element;
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+    }
+
+    public E delete(SLLNode<E> node) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            if(first == node) {
+                return this.deleteFirst();
+            }
+            while (tmp.succ != node && tmp.succ.succ != null)
+                tmp = tmp.succ;
+            if (tmp.succ == node) {
+                tmp.succ = tmp.succ.succ;
+                return node.element;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+                return null;
+            }
+        } else {
+            System.out.println("Listata e prazna");
+            return null;
+        }
+
+    }
+
+    public SLLNode<E> getFirst() {
+        return first;
+    }
+
+    public SLLNode<E> find(E o) {
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while (!tmp.element.equals(o) && tmp.succ != null)
+                tmp = tmp.succ;
+            if (tmp.element.equals(o)) {
+                return tmp;
+            } else {
+                System.out.println("Elementot ne postoi vo listata");
+            }
+        } else {
+            System.out.println("Listata e prazna");
+        }
+        return null;
+    }
+
+    public void merge (SLL<E> in){
+        if (first != null) {
+            SLLNode<E> tmp = first;
+            while(tmp.succ != null)
+                tmp = tmp.succ;
+            tmp.succ = in.getFirst();
+        }
+        else{
+            first = in.getFirst();
+        }
+    }
+
+    public void mirror() {
+        if (first != null) {
+            //m=nextsucc, p=tmp,q=next
+            SLLNode<E> tmp = first;
+            SLLNode<E> newsucc = null;
+            SLLNode<E> next;
+
+            while(tmp != null){
+                next = tmp.succ;
+                tmp.succ = newsucc;
+                newsucc = tmp;
+                tmp = next;
+            }
+            first = newsucc;
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SLL<String> niza = new SLL<>();
+        int N = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < N; i++) {
+            niza.insertLast(sc.next());
+        }
+        int L = sc.nextInt();
+        System.out.println(niza.toString());
+        SLLNode<String> dvizi = niza.getFirst();
+      
+        for(int i = 0;i<N;i++)
+        {
+            if(dvizi.element.length()==L){
+                 SLLNode<String> brisi = dvizi;
+                niza.delete(brisi);
+                niza.insertLast(brisi.element);
+            }
+            dvizi=dvizi.succ;
+        }
+        System.out.println(niza.toString());
+}
+}
+
